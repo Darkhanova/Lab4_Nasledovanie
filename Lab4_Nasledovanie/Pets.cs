05cfe0fe1781790a1f962ec2a5be872703d9534c
Darkhanova <91181096+Darkhanova@users.noreply.github.com>
2021-11-08 20:39:56 +0800

    добавлено общее свойство(вес)
    одно свойство передающееся по наследству остальным

--- Lab4_Nasledovanie/Pets.cs
+++ Lab4_Nasledovanie/Pets.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,28 +8,30 @@ namespace Lab4_Nasledovanie
 {
     public class Pets
     {
+        public double weight;
         public virtual String GetInfo()
-        {
+        { 
             return "";
         }
     }
-
 
     public class Cow : Pets
     {
         public int horn = 0;
         public int milk = 0;
-
         public override String GetInfo()
         {
+            var rnd = new Random();
+            weight = 600 - rnd.Next() % 401; //Ср. вес коровы 200-600кг
             var str = "Корова";
+            str += String.Format("\nВес: {0}", weight + "кг.");
             str += String.Format("\nДлина Рогов: {0}", this.horn + " см.");
             str += String.Format("\nМолока/сутки: {0}", this.milk + " л.");
             return str;
         }
     }
 
-    public enum DogType {Бульдог, ЧихуаХуа, Хаски, Волкодав, Лабрадор, НемОвчарка};
+    public enum DogType {Бульдог, Борзая, Хаски, Волкодав, Лабрадор, НемОвчарка};
     public class Dog : Pets
     {
         public int lengthtail = 0;
@@ -37,7 +39,10 @@ namespace Lab4_Nasledovanie
         public DogType type = DogType.Хаски;
         public override String GetInfo()
         {
+            var rnd = new Random();
+            weight = 40 - rnd.Next() % 16;//25-40kg
             var str = "Собака";
+            str += String.Format("\nВес: {0}", weight + "кг.");
             str += String.Format("\nДлина хвоста: {0}", this.lengthtail + " см.");
             str += String.Format("\nДлина игнора команд: {0}", this.lengthcommands + " cм.");
             str += String.Format("\nПорода: {0}", this.type);
@@ -51,8 +56,12 @@ namespace Lab4_Nasledovanie
         public int killsmouse = 0;
         public override String GetInfo()
         {
+            var rnd = new Random();
+            weight = 45 - rnd.Next() % 10;//3.6-4.5kg
+            weight = weight / 10;//Получение дроби
             var str = "Кошка";
-            if(fur == false)
+            str += String.Format("\nВес: {0}", weight + "кг.");
+            if (fur == false)
                 str += String.Format("\nШерсть: Нет");
             if (fur == true)
                 str += String.Format("\nШерсть: Есть");
@@ -61,7 +70,4 @@ namespace Lab4_Nasledovanie
             return str;
         }
     }
-
-
-
 }
